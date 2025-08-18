a452b22f04dd6c026c35cc9822cb85b57b4f0691
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2025-08-18 11:25:26 +0100

    Update mining_optimized.js

--- patterns/gachaModes/mining_optimized.js
+++ patterns/gachaModes/mining_optimized.js
@@ -845,7 +845,7 @@ module.exports = async (channel, dbEntry, json, client) => {
                         // 1% chance to find something on the floor.
                         if (Math.random() * 100 > 99.9) {
                             const item = pickWeightedItem(powerLevel);
-                            eventLogs.push(`${member.displayName}'s will and determination broke through! They mined the ore with their raw fists!!! ${item.name} x${quantity} found`);
+                            eventLogs.push(`🔥 ${member.displayName}'s will and determination broke through! They mined the ore with their raw fists!!! ${item.name} x${quantity} found`);
                             await addItemToMinecart(dbEntry, member.id, item.itemId, quantity);
                         }else {
                             eventLogs.push(`${member.displayName} tried to mine ore but has no pickaxe. They shook their fists helplessly...`);
