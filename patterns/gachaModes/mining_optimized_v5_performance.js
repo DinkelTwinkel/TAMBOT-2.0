ffad72f204e0900314cfa7104e0680e533191058
Jason <ext-jason.zhaoshen-yao@supercell.com>
2025-09-14 03:29:50 +0300

    Update mining_optimized_v5_performance.js

--- patterns/gachaModes/mining_optimized_v5_performance.js
+++ patterns/gachaModes/mining_optimized_v5_performance.js
@@ -4448,6 +4448,7 @@ async function processPlayerActionsEnhanced(member, playerData, mapData, powerLe
                     if ([TILE_TYPES.WALL_WITH_ORE, TILE_TYPES.RARE_ORE].includes(targetTile.type)) {
                         console.log(`[MINE ID DEBUG] ${member.displayName} SPEED mining with mineTypeId: "${mineTypeId}" (type: ${typeof mineTypeId}) | tile type: ${targetTile.type}`);
                         console.log(`[STAT DEBUG] gameStatTracker exists: ${!!gameStatTracker}, tile type: ${targetTile.type}, WALL_WITH_ORE: ${TILE_TYPES.WALL_WITH_ORE}, RARE_ORE: ${TILE_TYPES.RARE_ORE}`);
+                        console.log(`[STAT DEBUG] Condition check: ${[TILE_TYPES.WALL_WITH_ORE, TILE_TYPES.RARE_ORE].includes(targetTile.type)}`);
                         const { item, quantity, destination } = await mineFromTile(member, miningPower, luckStat, powerLevel, targetTile.type, availableItems, efficiency, isDeeperMine, mineTypeId);
                         
                         let finalQuantity = quantity;
